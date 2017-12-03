f50863ee35992bf22203ce29e6721efe97449833
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-03 09:24:02 +0000

    refactor #get_details

--- lib/bestselling_books_by_genre/cli.rb
+++ lib/bestselling_books_by_genre/cli.rb
@@ -9,30 +9,24 @@ class BestsellingBooksByGenre::CLI
     genre = gets.strip
   end
 
-  def list_books
+  def separate_genre_arrays
     if genre == "fiction"
       @fiction_books = BestsellingBooksByGenre::Book.scrape_fiction
-      @fiction_books.each.with_index(1) do |book, i|
-        puts "#{i}. #{book.title} by #{book.author}"
-      end
     elsif genre == "nonfiction"
       @fiction_books = BestsellingBooksByGenre::Book.scrape_nonfiction
-      @nonfiction_books.each.with_index(1) do |book, i|
-        puts "#{i}. #{book.title} by #{book.author}"
-      end
     end
   end
 
+  def list_books
+    separate_genre_arrays.each.with_index(1) do |book, i|
+      puts "#{i}. #{book.title} by #{book.author}"
+  end
+
   def get_details
-    if genre == "fiction"
-      book_array = @fiction_books
-    elsif genre == "nonfiction"
-      book_array = @nonfiction_books
-    end
     puts "Please enter the number of the book you'd like to learn more about (or type exit):"
     input = gets.strip
     if input.to_i.between?(1,5)
-      book = book_array[input.to_i - 1]
+      book = separate_genre_arrays[input.to_i - 1]
       puts "#{book.summary}"
       #fix later
       #puts "To purchase this book on Amazon, visit: #{book.link_to_buy}"
