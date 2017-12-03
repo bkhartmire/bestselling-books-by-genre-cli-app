107db46c385ead37690dbf6b2dcf33b96a2c5bb4
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-03 08:07:45 +0000

    start #list_books

--- lib/bestselling_books_by_genre/book.rb
+++ lib/bestselling_books_by_genre/book.rb
@@ -1,17 +1,19 @@
+require 'pry'
+
 class BestsellingBooksByGenre::Book
   attr_accessor :genre, :title, :author, :summary, :link_to_buy
 
-  def self.
-    books = []
-  end
+
 
   def self.scrape_nytimes
     doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
     book = self.new
-    book.title = doc.search(".book-body.title").text.strip
-    book.author = doc.search(".book-body p.author").text.strip.gsub("by ", "")
+    book.title = doc.search(".book-body h3.title").text.strip
+    book.author = doc.search(".book-body p.author").text.gsub("by ", "").strip
     book.summary = doc.search(".book-body p.description").text.strip
-    book.link_to_buy = doc.search(".book-body.menu li").first.attr("href").strip
+    #Fix this code later:
+    #book.link_to_buy = doc.css("footer.book-footer a").first.attr("href").value
     book
+
   end
 end
--- lib/bestselling_books_by_genre/cli.rb
+++ lib/bestselling_books_by_genre/cli.rb
@@ -2,4 +2,18 @@ class BestsellingBooksByGenre::CLI
   def call
     puts "Hello"
   end
+
+  def list_books
+    puts "Would you like to view the NY Times top 5 bestselling fiction or nonfiction books?"
+    genre = gets.strip
+    if genre == "fiction"
+      @fiction_books.each.with_index(1) do |book, i|
+        puts "#{i}. #{book.title} by #{book.author}"
+      end
+    elsif genre == "nonfiction"
+      @nonfiction_books.each.with_index(1) do |book, i|
+        puts "#{i}. #{book.title} by #{book.author}"
+      end
+    end
+  end
 end
