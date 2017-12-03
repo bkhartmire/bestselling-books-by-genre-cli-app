4b764af12f94c21b1fcd6bf68a3741c6a76aa90f
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-03 07:26:10 +0000

    scrape_nytimes

--- lib/bestselling_books_by_genre/book.rb
+++ lib/bestselling_books_by_genre/book.rb
@@ -8,10 +8,10 @@ class BestsellingBooksByGenre::Book
   def self.scrape_nytimes
     doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
     book = self.new
-    book.title = doc.search("").text.strip
-    book.author = doc.search("").text.strip
-    book.summary = doc.search("").text.strip
-    book.link_to_buy = doc.search("").attr("href").strip
+    book.title = doc.search(".book-body.title").text.strip
+    book.author = doc.search(".book-body p.author").text.strip.gsub("by ", "")
+    book.summary = doc.search(".book-body p.description").text.strip
+    book.link_to_buy = doc.search(".book-body.menu li").first.attr("href").strip
     book
   end
 end
