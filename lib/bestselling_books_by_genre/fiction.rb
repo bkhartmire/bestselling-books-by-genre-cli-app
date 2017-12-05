2556021efa54d29a2dcfe04e56343e50f17933fa
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-05 03:20:01 +0000

    fix scrape_nonfiction and move attr methods into nonfiction_bestsellers

--- lib/bestselling_books_by_genre/fiction.rb
+++ lib/bestselling_books_by_genre/fiction.rb
@@ -11,37 +11,27 @@ class BestsellingBooksByGenre::Fiction
 
   def self.scrape_fiction
     doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
-    doc.css("section.subcategory")[0].search(".book-body")
+    doc.css("section.subcategory")[0].search("article.book")
   end
 
   def self.fiction_bestsellers
-    scrape_fiction.each do |book_body|
+    scrape_fiction.each do |article|
       book = self.new
-      book.title = book_body.search("h3.title").text.strip
-      book.author = book_body.search("p.author").text.gsub("by ", "").strip
-      book.summary = book_body.search("p.description").text.strip
-      binding.pry
-      book.link_to_buy = book_body.search("footer.book-footer a").first.attr("href").value
       @@all << book
+      book.title = article.search("h3.title").text.strip
+      book.author = article.search("p.author").text.gsub("by ", "").strip
+      book.summary = article.search("p.description").text.strip
+      #book.link_to_buy = article.search("footer.book-footer a").first.attr("href").value
+
     end
   end
 
-#  def title
-#    @title = self.class.scrape_fiction.search("h3.title").text.strip
-#  end
 
-#  def author
-#    @author = self.class.scrape_fiction.search(".book-body p.author").text.gsub("by ", "").strip
-#  end
-
-  def summary
-    @summary = self.class.scrape_fiction.search(".book-body p.description").text.strip
-  end
 
   #Fix this code later:
-  def link_to_buy
-    @link_to_buy = self.class.scrape_fiction.search("footer.book-footer a").first.attr("href").value
-  end
+  #def link_to_buy
+  #  @link_to_buy = self.class.scrape_fiction.search("footer.book-footer a").first.attr("href").value
+#  end
 
 
 end
--- lib/bestselling_books_by_genre/nonfiction.rb
+++ lib/bestselling_books_by_genre/nonfiction.rb
@@ -9,32 +9,24 @@ class BestsellingBooksByGenre::Nonfiction
     @@all
   end
 
+  def self.scrape_nonfiction
+    doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
+    doc.css("section.subcategory")[2].search("article.book")
+  end
+
   def self.nonfiction_bestsellers
-    scrape_nonfiction.each do |book|
+    scrape_nonfiction.each do |article|
       book = self.new
       @@all << book
+      book.title = article.search("h3.title").text.strip
+      book.author = article.search("p.author").text.gsub("by ", "").strip
+      book.summary = article.search("p.description").text.strip
     end
   end
 
-  def self.scrape_nonfiction
-    doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
-    doc.css("section.subcategory")[2]
-  end
-
-  def title
-    @title = self.class.scrape_nonfiction.search(".book-body h3.title").text.strip
-  end
-
-  def author
-    @author = self.class.scrape_nonfiction.search(".book-body p.author").text.gsub("by ", "").strip
-  end
-
-  def summary
-    @summary = self.class.scrape_nonfiction.search(".book-body p.description").text.strip
-  end
 
   #Fix this code later:
-  def link_to_buy
-    @link_to_buy = self.class.scrape_nonfiction.search("footer.book-footer a").first.attr("href").value
-  end
+  #def link_to_buy
+  #  @link_to_buy = self.class.scrape_nonfiction.search("footer.book-footer a").first.attr("href").value
+  #end
 end
