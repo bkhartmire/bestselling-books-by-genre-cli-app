717ee52e5a9d1ad72766ae6d574a2e54fd10b4fc
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-07 00:58:04 +0000

    merge scraper-class branch

--- lib/bestselling_books_by_genre/scraper.rb
+++ lib/bestselling_books_by_genre/scraper.rb
@@ -6,7 +6,7 @@ class BestsellingBooksByGenre::Scraper
     doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
     doc.css("section.subcategory")[0].search("article.book")
   end
-  
+
   def self.scrape_nonfiction
     doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
     doc.css("section.subcategory")[2].search("article.book")
