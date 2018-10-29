54cc868c08bc3896d682a07f4c14ebc42f27e37f
bkhartmire <bkhartmire@gmail.com>
2018-10-30 01:47:16 +0900

    require new book class

--- lib/bestselling_books_by_genre.rb
+++ lib/bestselling_books_by_genre.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'pry'
 
 require_relative "./bestselling_books_by_genre/version"
+require_relative './bestselling_books_by_genre/book'
 require_relative './bestselling_books_by_genre/fiction'
 require_relative './bestselling_books_by_genre/nonfiction'
 require_relative './bestselling_books_by_genre/scraper'
